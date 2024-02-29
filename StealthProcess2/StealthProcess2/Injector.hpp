9cfbe5fcbe91ff082aac2d26deffdad1c0fc58a3
ybeomgi <ybeomgi@gmail.com>
2024-02-29 23:26:18 +0900

    2024-02-29 스텔스가 안됨...

---
+++ StealthProcess2/StealthProcess2/Injector.hpp
@@ -0,0 +1,134 @@
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+#include <TlHelp32.h>
+
+#define BUFSIZE		1024
+
+#define PROCESS_TITLE	1
+#define EXEC_NAME		2
+#define PROCESS_ID		3
+
+BOOL CALLBACK EnumWindowsProc(_In_ HWND hwnd, _In_ LPARAM lParam);
+
+typedef class INJECTOR {
+private:
+	TCHAR processTitle[BUFSIZE] = { 0, };
+	DWORD pid = NULL;
+	HANDLE processHandle = NULL, remoteThreadHandle = NULL;
+	void* allocMemoryAddr = NULL;
+	TCHAR dllPath[MAX_PATH] = { 0, };
+	LPTHREAD_START_ROUTINE remoteThreadFunc = NULL;
+
+	void ErrorMessage(const char* msg) {
+		MessageBoxA(NULL, msg, "ERROR", NULL);
+		exit(1);
+	};
+
+public:
+	INJECTOR(TCHAR* title, int type = PROCESS_TITLE) {
+		if (title == NULL) ErrorMessage("Title is wrong");
+		wcscpy_s(processTitle, title);
+
+		if (type == PROCESS_TITLE) {
+			if (EnumWindows(EnumWindowsProc, (LPARAM)this)) printf_s("[INJECTOR] : Find Process \n");
+			if (pid == NULL) ErrorMessage("PID is not exist");
+			processHandle = OpenProcess(PROCESS_ALL_ACCESS, TRUE, pid);
+			if (processHandle == NULL) {
+				printf("Process Open Failed\n");
+			};
+		}
+		else if (type == EXEC_NAME) {
+			auto snap = CreateToolhelp32Snapshot(TH32CS_SNAPALL, NULL);
+			PROCESSENTRY32 pe32;
+			
+			memset(&pe32, 0, sizeof(PROCESSENTRY32));
+			pe32.dwSize = sizeof(PROCESSENTRY32);
+			
+			while (TRUE) {
+				if (pe32.th32ProcessID == NULL) 
+					if (!Process32First(snap, &pe32)) ErrorMessage("Process32First");
+				
+				auto result = _wcsicmp(title, pe32.szExeFile);
+				if (result == 0 && result != 0xffffffff) {
+					pid = pe32.th32ProcessID;
+					break;
+				};
+				Process32Next(snap, &pe32);
+			}
+		}
+
+		processHandle = OpenProcess(PROCESS_ALL_ACCESS, TRUE, pid);
+	};
+
+	INJECTOR(int pid, int type = PROCESS_TITLE) {
+		if (pid == NULL) ErrorMessage("PID is not exist");
+		this->pid = pid;
+		processHandle = OpenProcess(PROCESS_ALL_ACCESS, TRUE, pid);
+		if (processHandle == NULL) {
+			printf("Process Open Failed\n");
+		};
+	};
+
+	~INJECTOR() {
+		CloseHandle(this->processHandle);
+		CloseHandle(this->remoteThreadHandle);
+		TerminateThread(this->remoteThreadHandle, 0);
+	};
+
+	BOOL Injection() {
+		allocMemoryAddr = VirtualAllocEx(processHandle, NULL, BUFSIZE, MEM_COMMIT, PAGE_READWRITE);
+		if (allocMemoryAddr == NULL) ErrorMessage("Mem Alloc Failed");
+		if (!WriteProcessMemory(this->processHandle, this->allocMemoryAddr, this->dllPath, BUFSIZE, NULL)) ErrorMessage("Mem Write Failed");
+		auto modHandle = GetModuleHandle(L"kernel32.dll");
+		remoteThreadFunc = (LPTHREAD_START_ROUTINE)GetProcAddress(modHandle, "LoadLibraryW");
+		if (remoteThreadFunc == NULL) ErrorMessage("Function load Failed");
+
+		remoteThreadHandle = CreateRemoteThread(this->processHandle, NULL, 0, this->remoteThreadFunc, this->allocMemoryAddr, NULL, NULL);
+		if (remoteThreadHandle == NULL) ErrorMessage("Create Remote Thread Failed");
+
+		WaitForSingleObject(this->remoteThreadHandle, INFINITY);
+		return TRUE;
+	};
+
+	BOOL SetDLLPath(TCHAR* path) {
+		memset(this->dllPath, 0, MAX_PATH);
+		wcscpy_s(this->dllPath, path);
+		return true;
+	};
+	TCHAR* GetDLLPath() { return this->dllPath; };
+
+	void SetPID(DWORD PID) { this->pid = PID; };
+	DWORD GetPID() { return this->pid; };
+
+	TCHAR* GetTitle() { return this->processTitle; };
+
+}injector;
+
+BOOL CALLBACK EnumWindowsProc(_In_ HWND hwnd, _In_ LPARAM lParam) {
+	TCHAR currentTitle[BUFSIZE] = { 0, };
+	TCHAR title[BUFSIZE] = { 0, };
+
+	INJECTOR* injector = (INJECTOR*)lParam;
+
+	DWORD pid = NULL;
+
+
+	wcscpy_s(title, injector->GetTitle());
+
+	while (1) {
+		GetWindowText(hwnd, currentTitle, BUFSIZE);
+		//wprintf(L"%s", currentTitle, BUFSIZE);
+		auto result = wcscmp(currentTitle, title);
+		if (result == 0) {
+			GetWindowThreadProcessId(hwnd, &pid);
+			if (pid == NULL) return TRUE;
+
+			injector->SetPID(pid);
+			return FALSE;
+		}
+		hwnd = GetNextWindow(hwnd, GW_HWNDNEXT);
+	}
+	return TRUE;
+};
---
+++ StealthProcess2/StealthProcess2/dllmain.cpp
@@ -0,0 +1,109 @@
+﻿// dllmain.cpp : DLL 애플리케이션의 진입점을 정의합니다.
+#include "pch.h"
+
+#define JMP_BUFFER		6
+#define HOOKING_BUFFER	14 //JMP_BUFFER + DWORD64
+
+void ErrorMessage(const char* msg);
+void Hooking(const char* dllName, const char* procName, const BYTE* buffer, const int bufferSize, BYTE* oriBuffer = NULL);
+BOOL NewCreateProcessA(LPCSTR appName, LPSTR cmdLine, LPSECURITY_ATTRIBUTES procAttributes, LPSECURITY_ATTRIBUTES threadAttributes, BOOL inHeritHandle, DWORD flags, LPVOID env, LPCSTR currentDir, LPSTARTUPINFOA startupInfo, LPPROCESS_INFORMATION procInfo);
+BOOL WINAPI NewCreateProcessW(_In_opt_ LPCWSTR appName, _Inout_opt_ LPWSTR cmdLine, _In_opt_ LPSECURITY_ATTRIBUTES procAttributes, _In_opt_ LPSECURITY_ATTRIBUTES threadAttributes, _In_ BOOL inHeritHandle, _In_ DWORD flags, _In_opt_ LPVOID env, _In_opt_ LPCWSTR currentDir, _In_ LPSTARTUPINFOW startupInfo, _Out_ LPPROCESS_INFORMATION procInfo);
+
+typedef BOOL(WINAPI* procA)(LPCSTR appName, LPSTR cmdLine, LPSECURITY_ATTRIBUTES procAttributes, LPSECURITY_ATTRIBUTES threadAttributes, BOOL inHeritHandle, DWORD flags, LPVOID env, LPCSTR currentDir, LPSTARTUPINFOA startupInfo, LPPROCESS_INFORMATION procInfo);
+
+typedef BOOL(WINAPI* procW)(_In_opt_ LPCWSTR appName, _Inout_opt_ LPWSTR cmdLine, _In_opt_ LPSECURITY_ATTRIBUTES procAttributes, _In_opt_ LPSECURITY_ATTRIBUTES threadAttributes, _In_ BOOL inHeritHandle, _In_ DWORD flags, _In_opt_ LPVOID env, _In_opt_ LPCWSTR currentDir, _In_ LPSTARTUPINFOW startupInfo, _Out_ LPPROCESS_INFORMATION procInfo);
+
+TCHAR* DLL_PATH = (TCHAR*)L"C:\\Users\\bgyang\\Desktop\\sourcecode\\StealthProcess2\\StealthProcess2.dll";
+
+const char* dllName = "kernelbase";
+const char* procName = "CreateProcessA";
+DWORD64 procAddr = (DWORD64)NewCreateProcessA;
+
+const BYTE jmpCode[JMP_BUFFER] = { 0xff, 0x25, 0, };
+BYTE originBuffer[HOOKING_BUFFER] = { 0, };
+BYTE hookingCode[HOOKING_BUFFER] = { 0, };
+
+INJECTOR* ijt = nullptr;
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    
+
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        memcpy_s(hookingCode, JMP_BUFFER, jmpCode, JMP_BUFFER);
+        memcpy_s(&hookingCode[JMP_BUFFER], sizeof(DWORD64), &procAddr, sizeof(DWORD64));
+
+		Hooking(dllName, procName, hookingCode, HOOKING_BUFFER, originBuffer);
+		break;
+
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+void ErrorMessage(const char* msg) {
+	MessageBoxA(NULL, msg, "ERROR", NULL);
+	exit(1);
+};
+
+
+void Hooking(const char* dllName, const char* procName, const BYTE* buffer, const int bufferSize, BYTE* oriBuffer) {
+	HMODULE oriDll = NULL;
+	void* oriFunc = NULL;
+	DWORD oldProtect = 0;
+
+	oriDll = GetModuleHandleA(dllName);
+	if (oriDll == NULL) ErrorMessage("oriDll");
+
+	oriFunc = GetProcAddress(oriDll, procName);
+	if (oriFunc == NULL) ErrorMessage("oriFunc");
+
+
+	if (!VirtualProtect(oriFunc, bufferSize, PAGE_EXECUTE_READWRITE, &oldProtect)) ErrorMessage("VirtualProtect");
+
+	if (oriBuffer != NULL) memcpy_s(oriBuffer, bufferSize, oriFunc, bufferSize);
+	memcpy_s(oriFunc, bufferSize, buffer, bufferSize);
+
+	if (!VirtualProtect(oriFunc, bufferSize, oldProtect, &oldProtect));
+};
+
+BOOL NewCreateProcessA(LPCSTR appName, LPSTR cmdLine, LPSECURITY_ATTRIBUTES procAttributes, LPSECURITY_ATTRIBUTES threadAttributes, BOOL inHeritHandle, DWORD flags, LPVOID env, LPCSTR currentDir, LPSTARTUPINFOA startupInfo, LPPROCESS_INFORMATION procInfo) {
+	Hooking(dllName, procName, originBuffer, HOOKING_BUFFER);
+	auto result = CreateProcessA(appName, cmdLine, procAttributes, threadAttributes, inHeritHandle, flags, env, currentDir, startupInfo, procInfo);
+	Hooking(dllName, procName, hookingCode, HOOKING_BUFFER);
+
+	if (!result) return FALSE;
+
+	ijt = new INJECTOR(procInfo->dwProcessId, PROCESS_ID);
+	if (ijt == nullptr) ErrorMessage("Injector");
+
+	ijt->SetDLLPath(DLL_PATH);
+	ijt->Injection();
+
+	return TRUE;
+};
+
+BOOL WINAPI NewCreateProcessW(_In_opt_ LPCWSTR appName, _Inout_opt_ LPWSTR cmdLine, _In_opt_ LPSECURITY_ATTRIBUTES procAttributes, _In_opt_ LPSECURITY_ATTRIBUTES threadAttributes, _In_ BOOL inHeritHandle, _In_ DWORD flags, _In_opt_ LPVOID env, _In_opt_ LPCWSTR currentDir, _In_ LPSTARTUPINFOW startupInfo, _Out_ LPPROCESS_INFORMATION procInfo) {
+	
+	Hooking(dllName, procName, originBuffer, HOOKING_BUFFER);
+	auto result = CreateProcessW(appName, cmdLine, procAttributes, threadAttributes, inHeritHandle, flags, env, currentDir, startupInfo, procInfo);
+	Hooking(dllName, procName, hookingCode, HOOKING_BUFFER);
+
+	if (!result) return FALSE;
+
+	ijt = new INJECTOR(procInfo->dwProcessId, PROCESS_ID);
+	if (ijt == nullptr) ErrorMessage("Injector");
+
+	ijt->SetDLLPath(DLL_PATH);
+	ijt->Injection();
+
+	return TRUE;
+};
